6ac7a4d1888e0629ac0aa3532a15dff70e63fed8
Igor Sheko <irbisadm@gmail.com>
2020-06-19 16:12:57 +0300

    Update config.ts
    Update example settings

--- clent-web/src/config.ts
+++ clent-web/src/config.ts
@@ -1,5 +1,5 @@
-export const MY_APP = 'messaging.mobiledev';
-export const URL_NEW_USERS = 'http://demo.phpstend.internal/';
+export const MY_APP = 'app.example';
+export const URL_NEW_USERS = 'https://users.example.com/';
 export const TIME_ONLINE_NOTIFICATION = 30000;
 export const TIME_TYPING_NOTIFICATION = 10000;
 
